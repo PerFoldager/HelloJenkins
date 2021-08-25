2ae8eef398c3aa9fee03d9d9ac2a43beab594f9a
Per Foldager <PerFoldager@users.noreply.github.com>
2021-08-25 09:38:13 +0200

    Hello Java Code, Maven and Jenkinsfile

---
+++ src/helloapp/src/main/java/dk/helloapp/App.java
@@ -0,0 +1,13 @@
+package dk.helloapp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
